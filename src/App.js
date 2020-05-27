830a4e1dbb3db1736599176a761c7cf55b0b4098
Julien De Conti <julien.de.conti@gmail.com>
2020-05-27 16:24:37 +0200

    example: https://github.com/nicolodavis/boardgame.io/tree/master/examples/react-web

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
+/*
+ * Copyright 2017 The boardgame.io Authors.
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import _ from 'lodash';
+import LiNavLink from './li-navlink';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import routes from './routes';
+import './app.css';
 
-export default App;
+// CSS for the sidebar is taken from vue.css
+export const App = () => (
+  <Router>
+    <main>
+      <aside className="sidebar">
+        <div className="sidebar-nav" style={{ height: '90%' }}>
+          <ul>
+            {routes.map((route_category, idx) => (
+              <li key={idx}>
+                <p>{route_category.name}</p>
+                <ul>
+                  {route_category.routes.map((route, _idx) => (
+                    <LiNavLink
+                      key={`${idx}.${_idx}`}
+                      to={route.path}
+                      exact={true}
+                      activeClassName="active"
+                    >
+                      {route.text}
+                    </LiNavLink>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </aside>
+      <section className="content">
+        {_.flattenDeep(routes.map(route => route.routes)).map((route, idx) => (
+          <Route
+            key={idx}
+            exact
+            path={route.path}
+            component={route.component}
+          />
+        ))}
+      </section>
+    </main>
+  </Router>
+);
