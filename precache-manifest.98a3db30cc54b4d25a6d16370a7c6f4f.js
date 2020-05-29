3ffa8f95c9a08324bd0fed384d4d6cac61227e05
Julien De Conti <julien.de.conti@gmail.com>
2020-05-29 10:37:24 +0000

    Updates

---
+++ precache-manifest.98a3db30cc54b4d25a6d16370a7c6f4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1451b517273cb9de588c07a6dc786f2",
+    "url": "/palm-island/index.html"
+  },
+  {
+    "revision": "679e592edb2ccea42233",
+    "url": "/palm-island/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "46c6d73c251a119dee51",
+    "url": "/palm-island/static/js/2.6b0804b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/palm-island/static/js/2.6b0804b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679e592edb2ccea42233",
+    "url": "/palm-island/static/js/main.b40cbbcb.chunk.js"
+  },
+  {
+    "revision": "341ada26c1d1ed3519d1",
+    "url": "/palm-island/static/js/runtime-main.e1b84ab4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/palm-island/static/media/logo.5d5d9eef.svg"
+  }
+]);
